1fae5e6d81608b97100cd2db8f4f20c175b895ff
HustlerDev45 <abdoul4845@gmail.com>
2023-10-04 09:38:20 -0700

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,11 +16,12 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 		s1 = "";
 	else
-	for (len1 = 0; s1[len1]; len1++);
+		for (len1 = 0; s1[len1]; len1++);
+
 	if (s2 == NULL)
 		s2 = "";
 	else
-	for (len2 = 0; s2[len2]; len2++);
+		for (len2 = 0; s2[len2]; len2++);
 
 	str = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (str == NULL)
@@ -28,8 +29,10 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; i < len1; i++)
 		str[i] = s1[i];
+
 	for (j = 0; j < len2; j++)
 		str[i + j] = s2[j];
+
 	str[i + j] = '\0';
 
 	return (str);
