0ecc83f6974c5b10417d92135dc86df6fb3cfa2b
HustlerDev45 <abdoul4845@gmail.com>
2023-10-05 14:49:06 -0700

    update

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,19 +1,30 @@
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
 /**
- * @argc: number of arguments.
+ * main - adds integers.
+ * @argc: numbers of arguments.
  * @argv: array of arguments.
- * Return: 0 si succès, 1 sinon
+ *
+ * Return: 0 (success), 1 otherwise.
  */
 int main(int argc, char *argv[])
 {
 	int sum = 0;
 
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
 	for (int i = 1; i < argc; i++)
 	{
-		for (int j = 0; argv[i][j] != '\0'; j++)
+		for (int j = 0; j < strlen(argv[i]); j++)
 		{
-			if (argv[i][j] < '0' || argv[i][j] > '9')
+			if (!isdigit(argv[i][j]))
 			{
 				printf("Erreur\n");
 				return (1);
