88b5d2ca95acf727154145d981a34616089f4e28
HustlerDev45 <abdoul4845@gmail.com>
2023-10-10 19:35:58 -0700

    update

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/**
+ * struct dog - Structure for a dog.
+ * @name: char* variable.
+ * @age: float variable.
+ * @owner: char* variable.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
