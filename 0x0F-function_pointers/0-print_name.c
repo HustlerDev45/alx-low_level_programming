f28bfc6ed2a44b4e8f6312113c3ff7cbdc50fdec
HustlerDev45 <abdoul4845@gmail.com>
2023-10-11 19:11:34 -0700

    update

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,41 +5,13 @@
  * print_name - Print a name using a custom printing function.
  * @name: The name to be printed.
  * @f: A pointer to the custom printing function.
+ *
+ * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
 	{
 		f(name);
-	}
-}
-
-/**
- * print_standard - A standard name printing function.
- * @name: The name to be printed.
- */
-void print_standard(char *name)
-{
-	if (name != NULL)
-	{
-		printf("Name: %s\n", name);
-	}
-}
-
-/**
- * print_uppercase - An uppercase name printing function.
- * @name: The name to be printed.
- */
-void print_uppercase(char *name)
-{
-	if (name != NULL)
-	{
-		int i = 0;
-		while (name[i])
-		{
-			putchar(toupper(name[i]));
-			i++;
-		}
-		putchar('\n');
 	}
 }
