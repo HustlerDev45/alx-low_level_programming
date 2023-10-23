1c8226a7f95068a31a3dfd0e7642f1a4524131da
HustlerDev45 <abdoul4845@gmail.com>
2023-10-23 13:20:21 -0700

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all elements of a linked listint_t list.
+ * print_listint - prints all elements of a listint_t list.
  * @h: pointer at the start of the list.
  * Return: number of nodes.
  */
