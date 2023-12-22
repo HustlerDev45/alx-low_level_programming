3dc7f57e76761a314136a5aa0da19af5f12ec744
HustlerDev45 <abdoul4845@gmail.com>
2023-12-22 12:17:32 -0800

    task2-fixed

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -5,12 +5,9 @@
  * @key: The key to get the index of.
  * @size: Size of the array of the hash table.
  *
- * Return: Index.
+ * Return: Key index.
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 {
-	unsigned long int hash_value = hash_djb2(key);
-	unsigned long int index = hash_value % size;
-
-	return (index);
+	return (hash_djb2(key) % size);
 }
